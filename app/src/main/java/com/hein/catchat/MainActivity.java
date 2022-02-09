31ef999e77abb72338d84a26138d14db044fb2ff
Heinnanda1997 <heinnanda683@gmail.com>
2022-02-09 10:59:11 +0630

    first commit

---
+++ app/src/main/java/com/hein/catchat/MainActivity.java
@@ -0,0 +1,106 @@
+package com.hein.catchat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+// By implement mainactivity can listen which item clicked;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,
+                R.string.nav_open_drawer,
+                R.string.nav_close_drawer);
+
+        drawerLayout.addDrawerListener(toggle);
+        //d to synchronize
+        //the icon on the toolbar with the state of the drawer. This is because
+        //the icon changes when you click on it to open the drawer:
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+         //Register the activity with the
+        //navigation view as a listener.
+        navigationView.setNavigationItemSelectedListener(this);
+        //Use a fragment to transaction to display an instance of InboxFragment;
+        Fragment fragment = new InBoxFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.content_frame,fragment);
+        ft.commit();
+
+
+    }
+
+    //This method gets called when the user
+    //clicks on one of the items in the drawer.
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        Fragment fragment = null;
+        Intent intent = null;
+
+        switch(id) {
+            case R.id.nav_drafts:
+                fragment = new DraftsFragment();
+                break;
+            case R.id.nav_sent:
+                fragment = new SentItemsFragment();
+                break;
+            case R.id.nav_trash:
+                fragment = new TrashFragment();
+                break;
+            case R.id.nav_help:
+                intent = new Intent(this, HelpActivity.class);
+                break;
+            case R.id.nav_feedback:
+                intent = new Intent(this, FeedbackActivity.class);
+                break;
+            default:
+                fragment = new InBoxFragment();
+        }
+
+        if(fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        }else {
+            startActivity(intent);
+        }
+
+        //Close the drawer when user select the options of the drawer;
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //When the user presses the Back
+    //button, close the drawer if it’s open.
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
